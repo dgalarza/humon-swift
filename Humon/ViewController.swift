45f717bf203af9db3217a6ef9b7cbe24635751ce
Damian Galarza <damian@thoughtbot.com>
2015-04-10 14:04:15 -0400

    Initial commit

---
+++ Humon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Humon
+//
+//  Created by Damian Galarza on 4/10/15.
+//  Copyright (c) 2015 thoughtbot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
